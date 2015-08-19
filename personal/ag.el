6eba4fcca7dc1488a0f791043d41f1da690409a6
Scott Pfister <scott.pfister@gmail.com>
2015-08-19 10:01:12 -0500

    add ag and terraform modules

---
+++ personal/ag.el
@@ -0,0 +1,9 @@
+;;; ag -- Summary
+
+;;; Commentary:
+
+;;; Code:
+(prelude-require-package `ag)
+
+(provide 'ag)
+;;; ag.el ends here
---
+++ personal/terraform.el
@@ -0,0 +1,14 @@
+;;; terraform -- Summary
+
+;;; Commentary:
+
+;;; Code:
+(prelude-require-packages '(elixir-mode alchemist))
+
+(require 'terraform-mode)
+
+(custom-set-variables
+ '(terraform-indent-level 2))
+
+(provide 'terraform)
+;;; terraform.el ends here
