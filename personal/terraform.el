8fad4cfad96de0f4628a6c9249047a9addde56f4
Scott Pfister <scott.pfister@gmail.com>
2016-05-06 09:27:28 -0500

    fix cut-and-paste error, actually require terraform-mode

--- personal/terraform.el
+++ personal/terraform.el
@@ -3,9 +3,7 @@
 ;;; Commentary:
 
 ;;; Code:
-(prelude-require-packages '(elixir-mode alchemist))
-
-(require 'terraform-mode)
+(prelude-require-packages '(terraform-mode))
 
 (custom-set-variables
  '(terraform-indent-level 2))
