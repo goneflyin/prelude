7f300bf8fb6c119806d569d08ad7f9ebc19ea39e
Scott Pfister <scott.pfister@gmail.com>
2016-09-03 12:23:32 -0400

    initial elm mode

---
+++ personal/elm.el
@@ -0,0 +1,12 @@
+;;; elm -- Summary
+
+;;; Commentary:
+
+;;; Code:
+(prelude-require-packages '(elm-mode))
+
+;; (custom-set-variables
+;;  '(terraform-indent-level 2))
+
+(provide 'elm)
+;;; elm.el ends here
